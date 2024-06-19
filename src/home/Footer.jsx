387112ce7996fe9781a3091ade23afd328ba2781
jordandatuin <jordan.datuin22@gmail.com>
2024-06-19 15:14:14 +0800

    Change AOS animate in Footer page.

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="py-[64px]" data-aos="fade-up" data-aos-duration="1000">
+    <footer className="py-[64px]" data-aos="zoom-in" data-aos-duration="1000">
         <p className="text-center font-semibold text-white">© 2023 Melchisideck Jordan U. Datuin All rights reserved.</p>
     </footer>
   )
